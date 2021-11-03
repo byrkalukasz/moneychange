dbf616a02710643a9d42d0e958578f6fcc250e11
byrkalukasz <lukasz.byrka1@gmail.com>
2021-11-04 00:40:46 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <select name="value" id="currency">
+      <option v-for="item in list" v-bind:key="item.code">{{item.code}} kurs : {{item.mid}}</option>
+    </select>
+    <br>
+    <label for="PLN">Ile chcesz wymienić PLN : </label>
+    <input type="text" id="PLN"><br>
+    <button v-on:click="convert()">Przelicz</button>
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default{
+  data()
+  {
+    return {list:undefined}
+  },
+  methods:{
+    convert: function()
+    {
+      const cr = document.querySelector('#currency')
+      const mid = this.list[cr.selectedIndex].mid
+      const foreign = this.list[cr.selectedIndex].code
+      const val = document.querySelector('#PLN').value
+      const done = val / mid;
+        alert("Otrzymasz: " + done + " " + foreign);
+    }
+  },
+  mounted()
+  {
+    const url="http://api.nbp.pl/api/exchangerates/tables/A/?format=json"
+
+    axios.get(url).then((resp) => 
+    {this.list = resp.data[0].rates;
+        console.log(this.list[1])
+    });
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
